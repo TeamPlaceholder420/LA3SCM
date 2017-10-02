30b0e53b58aa9a0e0221b6310e6cc7f029f1ec2e
Omar Alhammadi <b00062027@aus.edu>
2017-10-02 14:21:56 +0400

    init

---
+++ src/la3app/LA3app.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package la3app;
+
+/**
+ *
+ * @author b00062027
+ */
+public class LA3app {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
